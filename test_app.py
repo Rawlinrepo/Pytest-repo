f88a99f1b0f9b57748c28cc5ad285c50f90bbc78
Rawlin <rawlinleitao08@gmail.com>
2025-06-26 01:16:14 +0530

    Unit Testing with Pytest

--- test_app.py
+++ test_app.py
@@ -7,10 +7,10 @@ def test_subtract():
     assert subtract(10, 4) == 6
 
 def test_subtract1():
-    assert subtract(10, 4) == 3
+    assert subtract(10, 4) == 6
 
 def test_fail():
     assert subtract(5, 5) == 0  # This will FAIL on purpose to show red test
 
 def test_fail1():
-    assert subtract(5, 5) == 5
+    assert subtract(5, 5) == 0
